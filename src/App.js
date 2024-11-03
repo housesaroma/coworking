380833fe0792614a329c54d0e96006da5e92b53d
housesaroma <yahakulikov@yandex.ru>
2024-11-03 18:38:02 +0500

    header adaptive + footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './styles/App.css';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import "./styles/App.css";
+import "./styles/fonts.css";
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header title={"Главная"} />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
