362896c1b628dfce1b8dac6cd726cda9275abc69
housesaroma <yahakulikov@yandex.ru>
2024-11-04 20:20:04 +0500

    footer adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import avatar from "../../assets/Avatar.jpg";
-import notification from "../../assets/IconRingNotification.svg";
+import avatar from "../../assets/header/Avatar.jpg";
+import notification from "../../assets/header/IconRingNotification.svg";
 import cl from "./Header.module.css";
 
 const Header = ({ title }) => {
