4fb785439f31c393f8f77aa421ee38398fc8c128
housesaroma <yahakulikov@yandex.ru>
2024-11-15 12:55:57 +0500

    decompose bookings

---
+++ src/components/MyBookings/MyBookings.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import card1 from "../../assets/cards/card1.jpg";
+import card2 from "../../assets/cards/card2.jpg";
+import card3 from "../../assets/cards/card3.jpg";
+import CoworkingCard from "../../components/CoworkingCard/CoworkingCard";
+import arrow from "../../assets/icons/arrow.svg";
+import cl from "./MyBooking.module.css";
+
+const MyBookings = () => {
+    const [expandedSection, setExpandedSection] = useState(null);
+
+    const coworkingSpaces = [
+        { src: card1, title: "Территория идей", places: 15 },
+        { src: card2, title: "Территория столов", places: 30 },
+        { src: card3, title: "Территория светильников", places: 25 },
+    ];
+
+    const toggleSection = (section) => {
+        setExpandedSection(expandedSection === section ? null : section);
+    };
+
+    return (
+        <div>
+            <h1 className={cl.title}>Мои бронирования</h1>
+            <div className={cl.accordion}>
+                {[
+                    "Сегодня",
+                    "Завтра",
+                    "На этой неделе",
+                    "В ближайший месяц",
+                ].map((section, index) => (
+                    <div
+                        key={index}
+                        className={cl.accordionItem}
+                        onClick={() => toggleSection(section)}
+                    >
+                        <div className={cl.accordionHeader}>
+                            {section}
+                            <img
+                                src={arrow}
+                                alt="arrow"
+                                width={"20px"}
+                                height={"20px"}
+                                className={cl.arrow}
+                            ></img>
+                        </div>
+                        {expandedSection === section && (
+                            <div className={cl.cardContainer}>
+                                {coworkingSpaces.map((space, index) => (
+                                    <CoworkingCard
+                                        key={index}
+                                        src={space.src}
+                                        title={space.title}
+                                        subtitle={`Свободных мест осталось: ${space.places}`}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default MyBookings;
--- src/pages/BookingPage/BookingPage.jsx
+++ src/pages/BookingPage/BookingPage.jsx
@@ -1,15 +1,14 @@
 import React, { useContext, useState } from "react";
-import card1 from "../../assets/cards/card1.jpg";
-import card2 from "../../assets/cards/card2.jpg";
-import card3 from "../../assets/cards/card3.jpg";
+
 import { AuthContext } from "../../components/context";
-import CoworkingCard from "../../components/CoworkingCard/CoworkingCard";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import MyButton from "../../components/UI/Button/MyButton";
 import MyRadioButton from "../../components/UI/RadioButton/MyRadioButton";
 import cl from "./BookingPage.module.css";
-import arrow from "../../assets/icons/arrow.svg";
+import MyBookings from "../../components/MyBookings/MyBookings";
+import PersonalData from "../../components/PersonalData/PersonalData";
+import MyHistory from "../../components/MyHistory/MyHistory";
 
 export const BookingPage = () => {
     const { setIsAuth } = useContext(AuthContext);
@@ -20,17 +19,6 @@ export const BookingPage = () => {
     };
 
     const [title, setTitle] = useState("Мои бронирования");
-    const [expandedSection, setExpandedSection] = useState(null);
-
-    const coworkingSpaces = [
-        { src: card1, title: "Территория идей", places: 15 },
-        { src: card2, title: "Территория столов", places: 30 },
-        { src: card3, title: "Территория светильников", places: 25 },
-    ];
-
-    const toggleSection = (section) => {
-        setExpandedSection(expandedSection === section ? null : section);
-    };
 
     return (
         <div className={cl.bookingPage}>
@@ -55,44 +43,11 @@ export const BookingPage = () => {
                     История
                 </MyRadioButton>
             </div>
-            <h1 className={cl.title}>{title}</h1>
-            <div className={cl.accordion}>
-                {[
-                    "Сегодня",
-                    "Завтра",
-                    "На этой неделе",
-                    "В ближайший месяц",
-                ].map((section, index) => (
-                    <div
-                        key={index}
-                        className={cl.accordionItem}
-                        onClick={() => toggleSection(section)}
-                    >
-                        <div className={cl.accordionHeader}>
-                            {section}
-                            <img
-                                src={arrow}
-                                alt="arrow"
-                                width={"20px"}
-                                height={"20px"}
-                                className={cl.arrow}
-                            ></img>
-                        </div>
-                        {expandedSection === section && (
-                            <div className={cl.cardContainer}>
-                                {coworkingSpaces.map((space, index) => (
-                                    <CoworkingCard
-                                        key={index}
-                                        src={space.src}
-                                        title={space.title}
-                                        subtitle={`Свободных мест осталось: ${space.places}`}
-                                    />
-                                ))}
-                            </div>
-                        )}
-                    </div>
-                ))}
-            </div>
+
+            {title === "Мои бронирования" && <MyBookings />}
+            {title === "Личные данные" && <PersonalData/>}
+            {title === "История бронироования" && <MyHistory/>}
+            
             <div className={cl.exitBtn}>
                 <MyButton onClick={logout}>Выйти</MyButton>
             </div>
