8feb0e258fb3242ae1c213f20d36194823acc068
housesaroma <yahakulikov@yandex.ru>
2024-11-19 20:30:04 +0500

    notifications

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 import avatar from "../../assets/icons/header/Avatar.jpg";
 import notification from "../../assets/icons/header/IconRingNotification.svg";
+import MyButton from "../UI/Button/MyButton";
 import cl from "./Header.module.css";
-import { Navigate, useLocation } from "react-router-dom";
-import MyButton from '../UI/Button/MyButton'
 
 const Header = ({ title, showIcons }) => {
     const [redirectToBooking, setRedirectToBooking] = useState(false);
@@ -11,6 +11,16 @@ const Header = ({ title, showIcons }) => {
     const [redirectToScanner, setRedirectToScanner] = useState(false);
     const [showNotifications, setShowNotifications] = useState(false);
     const location = useLocation();
+
+    useEffect(() => {
+        let timer;
+        if (showNotifications) {
+            timer = setTimeout(() => {
+                setShowNotifications(false);
+            }, 5000); // Hide after 5 seconds
+        }
+        return () => clearTimeout(timer); // Clear timer on cleanup
+    }, [showNotifications]);
 
     const handleBookRedirect = () => {
         if (location.pathname !== "/booking") {
@@ -33,7 +43,7 @@ const Header = ({ title, showIcons }) => {
     }
 
     const toggleNotifications = () => {
-        setShowNotifications(prev => !prev);
+        setShowNotifications((prev) => !prev);
     };
 
     if (redirectToScanner) {
@@ -50,7 +60,9 @@ const Header = ({ title, showIcons }) => {
         <header className={cl.header}>
             <div className={cl.container}>
                 <div className={cl.left}>
-                    <a href onClick={handleMainRedirect} className={cl.logo}>B.ERP</a>
+                    <a href onClick={handleMainRedirect} className={cl.logo}>
+                        B.ERP
+                    </a>
                     <div className={cl.title}>Бронирование коворкингов</div>
                     <div className={cl.subtitle}>{title}</div>
                 </div>
@@ -79,17 +91,32 @@ const Header = ({ title, showIcons }) => {
             {showNotifications && (
                 <div className={cl.notificationsPopup}>
                     <div className={cl.notificationItem}>
-                        <p>Уведомление 1</p>
-                        <MyButton onClick={handleScannerRedirect}>Перейти к сканированию QR-кода</MyButton>
+                        <p className={cl.notificationText}>
+                            У вас забронирован коворкинг через 15 минут,
+                            отсканируйте qr-код на входной двери для входа в
+                            аудиторию
+                        </p>
+                        <div className={cl.btn}>
+                            <MyButton onClick={handleScannerRedirect}>
+                                Перейти к сканированию QR-кода
+                            </MyButton>
+                        </div>
                     </div>
                     <div className={cl.notificationItem}>
-                        <p>Уведомление 2</p>
-                        <MyButton onClick={handleScannerRedirect}>Перейти к сканированию QR-кода</MyButton>
+                        <p className={cl.notificationText}>
+                            У вас забронирован коворкинг через 15 минут,
+                            отсканируйте qr-код на входной двери для входа в
+                            аудиторию
+                        </p>
+                        <div className={cl.btn}>
+                            <MyButton onClick={handleScannerRedirect}>
+                                Перейти к сканированию QR-кода
+                            </MyButton>
+                        </div>
                     </div>
                     {/* Добавьте дополнительные уведомления по необходимости */}
                 </div>
             )}
-
         </header>
     );
 };
